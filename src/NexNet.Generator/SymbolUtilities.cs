09ef315ccca7306bff26a52f258b4f0ea1aa4c9d
DJGosnell <DJGosnell@users.noreply.github.com>
2023-05-23 19:24:47 -0400

    Added Nullable Returns and Params (#7)

---
+++ src/NexNet.Generator/SymbolUtilities.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+
+namespace NexNet.Generator;
+
+internal class SymbolUtilities
+{
+    private static readonly ConcurrentBag<StringBuilder> _sbCache = new ConcurrentBag<StringBuilder>();
+
+    public static StringBuilder GetStringBuilder()
+    {
+        if (!_sbCache.TryTake(out var sb))
+            sb = new StringBuilder();
+
+        return sb;
+    }
+
+    public static void ReturnStringBuilder(StringBuilder sb)
+    {
+        sb.Clear();
+        _sbCache.Add(sb);
+    }
+
+    private static readonly SymbolDisplayFormat _symbolDisplayFormat = new SymbolDisplayFormat(
+        globalNamespaceStyle: SymbolDisplayGlobalNamespaceStyle.Included,
+        typeQualificationStyle: SymbolDisplayTypeQualificationStyle.NameAndContainingTypesAndNamespaces,
+        genericsOptions: SymbolDisplayGenericsOptions.IncludeTypeParameters,
+        miscellaneousOptions: SymbolDisplayMiscellaneousOptions.EscapeKeywordIdentifiers
+                              | SymbolDisplayMiscellaneousOptions.UseErrorTypeSymbolName);
+
+    public static string GetFullSymbolType(ITypeSymbol typeSymbol, bool extractValueTask)
+    {
+        if (extractValueTask)
+        {
+            if (typeSymbol is INamedTypeSymbol namedTypeSymbol)
+            {
+                if (namedTypeSymbol.Arity == 1
+                    && namedTypeSymbol.ConstructedFrom.MetadataName == "ValueTask`1")
+                {
+                    typeSymbol = namedTypeSymbol.TypeArguments[0];
+                }
+                
+            }
+        }
+
+        var sb = GetStringBuilder();
+        sb.Append(typeSymbol.ToDisplayString(NullableFlowState.NotNull, _symbolDisplayFormat));
+
+        if (typeSymbol.TypeKind == TypeKind.Array
+            && typeSymbol.NullableAnnotation == NullableAnnotation.Annotated)
+            sb.Append("?");
+
+        if (sb.Length == 0)
+            return "UNKNOWN TYPE " + typeSymbol.ToString();
+
+        var result = sb.ToString();
+        ReturnStringBuilder(sb);
+        return result;
+    }
+}
