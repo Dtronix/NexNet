faacb8a59717c01868a488cae8f8130078167fea
DJGosnell <DJGosnell@gmail.com>
2023-08-03 16:22:22 -0400

    Added messenger sample.

--- src/NexNetDemo/Program.cs
+++ src/NexNetDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using NexNetDemo.Samples.InvocationSample;
+using NexNetDemo.Samples.Messenger;
 
 namespace NexNetDemo;
 
@@ -8,7 +9,24 @@ internal class Program
     {
         //await new DuplexPipeSimpleSample().UploadSample();
         //await new DuplexPipeStreamingSample().DuplexStreamingSample();
-        await new InvocationSample().UpdateInfo();
+        //await new InvocationSample().UpdateInfo();
+
+        var messengerSample = new MessengerSample("192.168.2.110");
+
+        if (args.Length == 1 && args[0] == "server")
+        {
+            await messengerSample.RunServer();
+        }
+        else if (args.Length == 1 && args[0] == "client")
+        {
+            await messengerSample.RunClient();
+        }
+        else
+        {
+            _ = Task.Factory.StartNew(() => messengerSample.RunServer(), TaskCreationOptions.LongRunning);
+            await messengerSample.RunClient();
+        }
+
 
 
         Console.ReadLine();
