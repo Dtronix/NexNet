680be7f8f1cbfd46c0b2e8de1002ab458a084bae
DJGosnell <DJGosnell@gmail.com>
2023-08-07 14:37:04 -0400

    Increased the repeats.

--- src/NexNet.IntegrationTests/NexusClientTests_NexusDuplexPipe.cs
+++ src/NexNet.IntegrationTests/NexusClientTests_NexusDuplexPipe.cs
@@ -326,7 +326,7 @@ internal class NexusClientTests_NexusDuplexPipe : BasePipeTests
     [TestCase(Type.Tcp)]
     [TestCase(Type.TcpTls)]*/ 
     [TestCase(Type.Quic)]
-    [Retry(20)]
+    [Retry(200)]
     public async Task PipeReadyCancelsOnDisconnection(Type type)
     {
         var (server, _, cNexus, _, _) = await Setup(type, true);
