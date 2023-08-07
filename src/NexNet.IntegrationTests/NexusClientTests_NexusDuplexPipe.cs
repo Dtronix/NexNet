4a217deec89dd6e1345c30aeef0186161f3d5632
DJGosnell <DJGosnell@gmail.com>
2023-08-07 14:38:40 -0400

    Actually added the repeat attributte.

--- src/NexNet.IntegrationTests/NexusClientTests_NexusDuplexPipe.cs
+++ src/NexNet.IntegrationTests/NexusClientTests_NexusDuplexPipe.cs
@@ -326,7 +326,7 @@ internal class NexusClientTests_NexusDuplexPipe : BasePipeTests
     [TestCase(Type.Tcp)]
     [TestCase(Type.TcpTls)]*/ 
     [TestCase(Type.Quic)]
-    [Retry(200)]
+    [Repeat(20)]
     public async Task PipeReadyCancelsOnDisconnection(Type type)
     {
         var (server, _, cNexus, _, _) = await Setup(type, true);
