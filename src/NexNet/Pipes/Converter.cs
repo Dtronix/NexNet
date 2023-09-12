b0f258873cf2fff5a9f628e849e685c956b29f97
DJGosnell <DJGosnell@gmail.com>
2023-09-12 18:11:15 -0400

    First iteration of channel converters.

---
+++ src/NexNet/Pipes/Converter.cs
@@ -0,0 +1,10 @@
+﻿namespace NexNet.Pipes;
+
+/// <summary>
+/// The Converter class in the NexNet.Pipes namespace is a generic class that provides functionality to convert objects of one type to another.
+/// The class takes two type parameters. The first type parameter represents the type of the object that will be converted, 
+/// and the second type parameter represents the type the object will be converted to.
+/// </summary>
+/// <typeparam name="TInput">The type of the object that will be converted.</typeparam>
+/// <typeparam name="TOutput">The type the object will be converted to.</typeparam>
+public delegate TOutput Converter<TInput, out TOutput>(in TInput input);
