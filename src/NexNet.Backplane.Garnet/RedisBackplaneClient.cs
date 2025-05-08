2e9805262345abb9b1f317931edb2cd87bad7722
DJGosnell <djgosnell@gmail.com>
2025-05-07 20:26:28 -0400

    Continued setup.

--- src/NexNet.Backplane.Garnet/RedisBackplaneClient.cs
+++ src/NexNet.Backplane.Garnet/RedisBackplaneClient.cs
@@ -1,7 +1,18 @@
-﻿namespace NexNet.Backplane.Redis;
+﻿using Garnet;
+using Garnet.client;
+using Garnet.server;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Console;
+
+namespace NexNet.Backplane.Garnet;
 
 public class RedisBackplaneClient : IBackplaneClient
 {
+
+    public RedisBackplaneClient()
+    {
+
+    }
     public ValueTask ConnectAsync()
     {
         throw new NotImplementedException();
--- src/NexNet/Backplane/IBackplaneClient.cs
+++ src/NexNet/Backplane/IBackplaneClient.cs
@@ -6,4 +6,6 @@ public interface IBackplaneClient
 {
     ValueTask ConnectAsync();
     ValueTask DisconnectAsync();
+    
+    
 }
