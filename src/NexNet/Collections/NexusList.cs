079d6296a9a57e28d974ed863769188356c777a2
djgosnell <djgosnell@gmail.com>
2025-05-19 17:19:35 -0400

    Started work on Lists & dictionaries.

---
+++ src/NexNet/Collections/NexusList.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using NexNet.Invocation;
+
+namespace NexNet.Collections;
+
+public class NexusList<T> : IList<T> 
+{
+    private readonly NexusDictionaryMode _mode;
+    private List<T> _list = new();
+    
+    internal NexusList(NexusDictionaryMode mode, IProxyInvoker invoker, int id)
+    {
+        _mode = mode;
+        //invoker.ProxyGetDuplexPipeInitialId()
+    }
+
+    /// <inheritdoc />
+    public IEnumerator<T> GetEnumerator()
+    {
+        return _list.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Add(T item)
+    {
+        _list.GetEnumerator();
+    }
+
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Contains(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void CopyTo(T[] array, int arrayIndex)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public bool Remove(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int Count { get; }
+    public bool IsReadOnly { get; }
+    public int IndexOf(T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Insert(int index, T item)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void RemoveAt(int index)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public T this[int index]
+    {
+        get => throw new System.NotImplementedException();
+        set => throw new System.NotImplementedException();
+    }
+}
---
+++ src/NexNet/Messages/CollectionUpdateMessage.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using MemoryPack;
+using NexNet.Cache;
+
+namespace NexNet.Messages;
+
+/// <summary>
+/// Contains an invocation request message data.
+/// </summary>
+[MemoryPackable(SerializeLayout.Explicit)]
+internal partial class CollectionUpdateMessage : IMessageBase
+{
+    /// <summary>
+    /// True if the message was deserialized from a pool.
+    /// </summary>
+    private bool _isArgumentPoolArray;
+    public static MessageType Type { get; } = MessageType.CollectionUpdate;
+
+    private ICachedMessage? _messageCache = null!;
+
+    [MemoryPackIgnore]
+    public ICachedMessage? MessageCache
+    {
+        set => _messageCache = value;
+    }
+
+    [MemoryPackOrder(0)]
+    public ushort CollectionId { get; set; }
+    
+    [MemoryPackOrder(1)]
+    public ushort StateId { get; set; }
+
+    [MemoryPackOrder(2)]
+    public ChangeType Change { get; set; }
+
+    [MemoryPackOrder(3)]
+    [MemoryPoolFormatter<byte>]
+    public Memory<byte> Arguments { get; set; }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public T? DeserializeArguments<T>()
+    {
+        return MemoryPackSerializer.Deserialize<T>(Arguments.Span);
+    }
+    
+    public bool TrySetArguments(ITuple? arguments)
+    {
+        Arguments = arguments == null
+            ? Memory<byte>.Empty
+            : MemoryPackSerializer.Serialize(arguments.GetType(), arguments);
+
+        //TODO: Review this on the sync path as it will get ignored as it is running on a separate task from the original caller.
+        return Arguments.Length <= IInvocationMessage.MaxArgumentSize;
+    }
+
+    [MemoryPackOnDeserialized]
+    private void OnDeserialized()
+    {
+        _isArgumentPoolArray = true;
+    }
+
+    public void Dispose()
+    {
+        var cache = Interlocked.Exchange(ref _messageCache, null);
+
+        if (cache == null)
+            return;
+
+        if (_isArgumentPoolArray)
+        {
+            // Reset the pool flag.
+            _isArgumentPoolArray = false;
+            IMessageBase.ReturnMemoryPackMemory(Arguments);
+            Arguments = default;
+        }
+
+        cache.Return(this);
+    }
+
+    public enum ChangeType
+    {
+        Unset,
+        Insert,
+        Remove,
+        Update,
+        Reset
+    }
+}
