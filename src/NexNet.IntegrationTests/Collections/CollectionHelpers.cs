c8f641f30426fa126e6e81b75c44a1341edfccb5
DJGosnell <djgosnell@gmail.com>
2025-07-21 21:24:24 -0400

    Removed debugging code.

--- src/NexNet.IntegrationTests/Collections/CollectionHelpers.cs
+++ src/NexNet.IntegrationTests/Collections/CollectionHelpers.cs
@@ -24,8 +24,6 @@ public class WaitForActionHandler : IDisposable
         {
             if (e.ChangedAction == action && Interlocked.Increment(ref currentCount) == count)
                 _tcs.SetResult();
-            
-            Console.WriteLine(currentCount);
         });
     }
     
