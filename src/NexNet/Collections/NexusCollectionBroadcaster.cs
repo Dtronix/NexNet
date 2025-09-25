b6faabc30faecb310d3bec8d268d64d16982deb8
djgosnell <djgosnell@gmail.com>
2025-09-25 12:55:45 -0400

    Initial rework of the broadcasting logic.

---
+++ src/NexNet/Collections/NexusCollectionBroadcaster.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using NexNet.Internals.Collections.Lists;
+using NexNet.Logging;
+
+namespace NexNet.Collections;
+
+
+internal class NexusCollectionBroadcaster
+{
+    private readonly SnapshotList<NexusCollectionClient> _connectedClients;
+    private readonly INexusLogger? _logger;
+    private readonly Channel<NexusBroadcastMessageWrapper> _messageBroadcastChannel;
+    
+    private bool _isRunning;
+    protected NexusCollectionBroadcaster(INexusLogger? logger)
+    {
+        _logger = logger?.CreateLogger("Broadcast");
+        _connectedClients = new SnapshotList<NexusCollectionClient>(64);
+        _messageBroadcastChannel = Channel.CreateBounded<NexusBroadcastMessageWrapper>(new BoundedChannelOptions(50)
+        {
+            AllowSynchronousContinuations = false,
+            FullMode = BoundedChannelFullMode.Wait,
+            SingleReader = true,
+            SingleWriter = false,
+        });
+    }
+
+    public void AddClientAsync(NexusCollectionClient client)
+    {
+        if(!_isRunning)
+            throw new InvalidOperationException("Broadcaster is not running.");
+        
+        // Add in the completion removal for execution later.
+        _logger?.LogTrace($"S{client.Session.Id} Starting collection message writer.");
+        
+        // Start the listener on the channel to handle sending updates to the client.
+        Task.Factory.StartNew(static async state =>
+        {
+            var client = (NexusCollectionClient)(state!);
+            NexusBroadcastMessageWrapper? wrapper = null;
+            try
+            {
+                await foreach (NexusBroadcastMessageWrapper messageWrapper in client.MessageSender.Reader.ReadAllAsync(client.CompletionToken).ConfigureAwait(false))
+                {
+                    wrapper = messageWrapper;
+                    var message = messageWrapper.SourceClient == client
+                        ? messageWrapper.SourceMessage ?? throw new Exception("Message to source client is null.")
+                        : messageWrapper.Message;
+
+                    await client.Writer!.WriteAsync(message, client.CompletionToken)
+                        .ConfigureAwait(false);
+                }
+            }
+            catch (Exception e)
+            {
+                client.Session.Logger?.LogInfo(e,
+                    $"S{client.Session.Id} Could not send collection broadcast message to session.");
+                // Ignore and disconnect.
+            }
+            finally
+            {
+                wrapper?.SignalCompletion();
+            }
+
+        }, client, TaskCreationOptions.DenyChildAttach);
+        
+        _connectedClients.Add(client);
+    }
+
+    public ValueTask BroadcastAsync(INexusCollectionMessage message, NexusCollectionClient? sourceClient)
+    {
+        var broadcastMessage = new NexusBroadcastMessageWrapper(sourceClient, message);
+        return _messageBroadcastChannel.Writer.WriteAsync(broadcastMessage);
+    }
+
+    public void Run(CancellationToken token)
+    {
+        if(_isRunning)
+            throw new InvalidOperationException("Broadcaster is already running.");
+        
+        Task.Factory.StartNew(async static args =>
+        {
+            var (broadcaster, ct) = ((NexusCollectionBroadcaster, CancellationToken))args!;
+            
+            broadcaster._logger?.LogTrace("Started broadcast loop.");
+            
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    await foreach (var broadcastMessage in broadcaster._messageBroadcastChannel.Reader.ReadAllAsync(ct).ConfigureAwait(false))
+                    {
+                        // Update the count.
+                        broadcastMessage.ClientCount = broadcaster._connectedClients.Count;
+                        foreach (var client in broadcaster._connectedClients)
+                        {
+                            try
+                            {
+                                if (!client.MessageSender.Writer.TryWrite(broadcastMessage))
+                                {
+                                    broadcaster._logger?.LogTrace(
+                                        $"S{client.Session.Id} Could not send to client collection");
+                                    // Complete the pipe as it is full and not writing to the client at a decent
+                                    // rate.
+                                    await client.Pipe.CompleteAsync().ConfigureAwait(false);
+                                    // Signal completion for this client since message won't be processed
+                                    broadcastMessage.SignalCompletion();
+                                }
+                                else
+                                {
+                                    broadcaster._logger?.LogTrace(
+                                        $"S{client.Session.Id} Sent to client collection");
+                                }
+                            }
+                            catch (Exception e)
+                            {
+                                broadcaster._logger?.LogInfo(e,
+                                    $"S{client.Session.Id} Exception while sending to collection. Removing from broadcast.");
+                                
+                                // If we threw, the client is disconnected. Remove the client and signal completion.
+                                broadcaster._connectedClients.Remove(client);
+                                broadcastMessage.SignalCompletion();
+                            }
+
+                        }
+                    }
+                    broadcaster._logger?.LogDebug("Stopped broadcast reading loop.");
+                }
+                catch (Exception e)
+                {
+                    broadcaster._logger?.LogError(e, "Exception in boradcast loop");
+                }
+            }
+        }, (this, token), TaskCreationOptions.DenyChildAttach);
+
+        _isRunning = true;
+    }
+}
+
+
+
+internal class NexusBroadcastMessageWrapper
+{
+    private int _completedCount = 0;
+    public int ClientCount;
+    public NexusCollectionClient? SourceClient { get; }
+    
+    /// <summary>
+    /// Message for the source client. Usually includes as Ack.
+    /// </summary>
+    public INexusCollectionMessage? SourceMessage { get; }
+    public INexusCollectionMessage Message { get; }
+
+    public NexusBroadcastMessageWrapper(NexusCollectionClient? sourceClient, INexusCollectionMessage message)
+    {
+        SourceClient = sourceClient;
+        Message = message;
+        
+        if (sourceClient != null)
+        {
+            SourceMessage = message.Clone();
+            SourceMessage.Flags |= NexusCollectionMessageFlags.Ack;
+        }
+    }
+
+    public void SignalCompletion()
+    {
+        if (Interlocked.Increment(ref _completedCount) == ClientCount)
+        {
+            SourceMessage?.ReturnToCache();
+            Message.ReturnToCache();
+        }
+    }
+}
---
+++ src/NexNet/Collections/NexusCollectionClient.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+using NexNet.Internals;
+using NexNet.Internals.Collections.Lists;
+using NexNet.Pipes;
+
+namespace NexNet.Collections;
+
+internal class NexusCollectionClient
+{
+    public readonly INexusDuplexPipe Pipe;
+    //public readonly INexusChannelReader<INexusCollectionMessage>? Reader;
+    public readonly INexusChannelWriter<INexusCollectionMessage>? Writer;
+    public readonly INexusSession Session;
+    public readonly Channel<NexusBroadcastMessageWrapper> MessageSender;
+
+    public StateType State;
+    
+    public CancellationToken CompletionToken;
+    
+
+    public NexusCollectionClient(INexusDuplexPipe pipe, 
+        INexusChannelWriter<INexusCollectionMessage>? writer,
+        INexusSession session)
+    {
+        Pipe = pipe;
+        State = StateType.Initializing;
+        Writer = writer;
+        Session = session;
+        MessageSender = Channel.CreateUnbounded<NexusBroadcastMessageWrapper>(new  UnboundedChannelOptions()
+        {
+            SingleReader = true,
+            SingleWriter = true, 
+        });
+        
+        var cts = new CancellationTokenSource();
+        CompletionToken = cts.Token;
+        
+        Pipe.CompleteTask.ContinueWith(
+            _ => cts.Cancel(),
+            CancellationToken.None,
+            TaskContinuationOptions.ExecuteSynchronously,
+            TaskScheduler.Default);
+
+    }
+        
+    public enum StateType
+    {
+        Unset,
+        AcceptingUpdates,
+        Initializing,
+        Disconnected
+    }
+}
